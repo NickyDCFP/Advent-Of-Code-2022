8ac72c370076547ee83520af635b74737396591b
Nicky Kriplani <94656320+NickyDCFP@users.noreply.github.com>
2022-12-02 17:38:41 -0500

    Add files via upload

---
+++ day2/day2_a.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <chrono>
+#define WIN_SCORE 6
+#define DRAW_SCORE 3
+#define LOSS_SCORE 0
+
+const std::string FILENAME = "day2_input.txt";
+const std::vector<int> result_score = {WIN_SCORE, LOSS_SCORE, DRAW_SCORE};
+const int n = 3;
+
+int main() {
+    auto start = std::chrono::high_resolution_clock::now();
+    std::ifstream f;
+    f.open(FILENAME);
+    std::string line;
+    int this_move, opp_move;
+    int score = 0;
+    while(std::getline(f, line)) {
+        this_move = (line[2] - 'X' + 1);
+        opp_move = (line[0] - 'A' + 1);
+        score += this_move;
+        score += result_score[(this_move + (n - 1) - opp_move) % n];
+        //score += (((this_move + (n - 1) - opp_move) % n + 2) % 3) * 3;
+    }
+    auto prog_time = std::chrono::duration_cast<std::chrono::microseconds>(
+                     std::chrono::high_resolution_clock::now() - start);
+    std::cout << "The final score would be " << score << ".\n" <<
+                 "Computed in " << (prog_time.count()) << " ms.";
+}
---
+++ day2/day2_b.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <chrono>
+#define WIN 0
+#define LOSE 1
+#define DRAW 2
+const std::string FILENAME = "day2_input.txt";
+const std::vector<int> outcome_mapping = {LOSE, DRAW, WIN}; 
+const int n = 3;
+
+int main() {
+    auto start = std::chrono::high_resolution_clock::now();
+    std::ifstream f;
+    f.open(FILENAME);
+    std::string line;
+    int this_move, opp_move, outcome;
+    int score = 0;
+    while(std::getline(f, line)) {
+        outcome = (line[2] - 'X');
+        opp_move = (line[0] - 'A' + 1);
+        score += outcome * 3;
+        this_move = (outcome_mapping[outcome] + opp_move) % n + 1;
+        score += this_move;
+    }
+    auto prog_time = std::chrono::duration_cast<std::chrono::microseconds>(
+                     std::chrono::high_resolution_clock::now() - start);
+    std::cout << "The final score would be " << score << ".\n" <<
+                 "Computed in " << (prog_time.count()) << " ms.";
+}
